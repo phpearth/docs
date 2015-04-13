24944daa92a5f7bc18d0a0f023cba36bec01ec89
Peter Kokot <peterkokot@gmail.com>
2015-04-13 14:22:31 +0200

    prepare generator.php script to generate search index, fix category in php-best-practices

--- generator.php
+++ generator.php
@@ -14,6 +14,7 @@ $parser = new Mni\FrontYAML\Parser();
 $groups = Yaml::parse(file_get_contents('./groups.yml'));
 
 $data = 'PHP Facebook group\'s frequently asked questions is a community driven project with answers to some frequently asked questions that are located on <a href="http://wwphp-fb.github.io/faq/">GitHub</a> for better code readability and better versioning capabilities. Please read these before asking questions here.<br><br>';
+$index  = '{"entries":[';
 
 foreach ($groups as $group) {
     $data .= '<strong>' . $group['title'] . '</strong>';
@@ -32,7 +33,9 @@ foreach ($groups as $group) {
 
             $faqs[$name] = array(
                 'title' => $yaml['title'],
-                'link' => 'http://wwphp-fb.github.io' . $yaml['permalink']
+                'link' => 'http://wwphp-fb.github.io' . $yaml['permalink'],
+                'updated' => '',
+                'body' => $document->getContent()
             );
         }
     }
@@ -41,6 +44,12 @@ foreach ($groups as $group) {
 
     foreach ($faqs as $faq) {
         $data .= '<li><a href="' . $faq['link'] . '">' . $faq['title'] . '</a></li>';
+        
+        $index .= '{"title": "' . $faq['title'] . '",
+            "url": "' . $faq['link'] . '",
+            "date": "' . $faq['updated'] . '",
+            "body": "' . $faq['title'] . '",
+            "categories": []},';
     }
 
     $data .= '</ul>';
@@ -50,3 +59,9 @@ $data .= '<strong>Want to get involved?</strong><br>We are always looking forwar
 This work is licensed under a Creative Commons Attribution 4.0 International License.';
 
 file_put_contents('./faq.html', $data, LOCK_EX);
+
+
+$index = rtrim($index, ',');
+$index .= ']}';
+
+file_put_contents('./indexdata.json', $index, LOCK_EX);
